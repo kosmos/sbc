9c54826b8cd7308b971d9a1c76ddeeb0d655828f
Kosmos <kosmos.ehp@gmail.com>
2018-11-28 17:33:59 +0200

    Update

---
+++ docs/precache-manifest.37b636fb2041e97a2bc1f8b9d4e4e168.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "b19fa5c6e8ddcb70078a1fff9a1fd94c",
+    "url": "img/BAT.b19fa5c6.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "6eb7f22203f1e0c6e9a256f25d62010c",
+    "url": "img/SYS.6eb7f222.svg"
+  },
+  {
+    "revision": "162aa6076e092c375936",
+    "url": "css/app.65145387.css"
+  },
+  {
+    "revision": "397d18fa971734eb91fb",
+    "url": "js/chunk-vendors.6eaaf72b.js"
+  },
+  {
+    "revision": "a96f0b36b60db5d2fc6b734e1bf8b357",
+    "url": "img/header.a96f0b36.jpg"
+  },
+  {
+    "revision": "881ee56683b0422dde7319a8febb3a33",
+    "url": "img/calc-bg.881ee566.jpg"
+  },
+  {
+    "revision": "0f17b25aad821761521d2d8b22d39761",
+    "url": "img/token.0f17b25a.jpg"
+  },
+  {
+    "revision": "1d2c82f9caff304fe2b5dd1d72209630",
+    "url": "fonts/Gilmer-Regular.1d2c82f9.woff2"
+  },
+  {
+    "revision": "8d5a96bef907eb8f9f69613b6df6a3b8",
+    "url": "fonts/Gilmer-Bold.8d5a96be.woff2"
+  },
+  {
+    "revision": "6ba66407adbeb4a540c57f010f193be0",
+    "url": "fonts/Gilmer-Medium.6ba66407.woff2"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "fonts/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "cea38e11924a21a9abb2783dfa4ee091",
+    "url": "fonts/Gilmer-Heavy.cea38e11.woff2"
+  },
+  {
+    "revision": "5c7d040943470ac1ff06937e1e717889",
+    "url": "fonts/Gilmer-Light.5c7d0409.woff2"
+  },
+  {
+    "revision": "b83589973e555afdd951415632737ff5",
+    "url": "img/VEE.b8358997.svg"
+  },
+  {
+    "revision": "4cb41226133dd3f4db0e7da343aedec5",
+    "url": "img/FYP.4cb41226.svg"
+  },
+  {
+    "revision": "c97ef11fe6c4c6355dbe0d426fad3f74",
+    "url": "img/PPC.c97ef11f.svg"
+  },
+  {
+    "revision": "d692baf03a48f6019ead4609bed8f929",
+    "url": "img/DOGE.d692baf0.svg"
+  },
+  {
+    "revision": "0b6be3356a9b9d0d232ccf76c829db18",
+    "url": "img/POWR.0b6be335.svg"
+  },
+  {
+    "revision": "d3ca211a18b5b0ac99e6ff4e54c7db85",
+    "url": "img/ZEC.d3ca211a.svg"
+  },
+  {
+    "revision": "114435cff592bd11c64385340bf587f8",
+    "url": "img/GRC.114435cf.svg"
+  },
+  {
+    "revision": "9d47cb8be33dc382cc8ae0f5815ce916",
+    "url": "img/VTC.9d47cb8b.svg"
+  },
+  {
+    "revision": "0784470071e1a768a23c",
+    "url": "js/about.37b7a025.js"
+  },
+  {
+    "revision": "162aa6076e092c375936",
+    "url": "js/app.7be5036b.js"
+  },
+  {
+    "revision": "a741154ec67c4e0cc0d7ab58569284d4",
+    "url": "img/ZRX.a741154e.svg"
+  },
+  {
+    "revision": "16f94822f234cbd4cf5b65a5640beb81",
+    "url": "img/DAI.16f94822.svg"
+  },
+  {
+    "revision": "c769cb5844fb74dd60e9a53352a7151f",
+    "url": "img/REP.c769cb58.svg"
+  },
+  {
+    "revision": "17f7ab23666cd7edeeebde5b4308a9fc",
+    "url": "img/GAME.17f7ab23.svg"
+  },
+  {
+    "revision": "bc0945fd708e1cd7b0db3f07267cd832",
+    "url": "img/ENJ.bc0945fd.svg"
+  },
+  {
+    "revision": "a9ba7a11f8d15fffbb9e02554ecd3645",
+    "url": "img/DASH.a9ba7a11.svg"
+  },
+  {
+    "revision": "e38efa7e5c4c52160bce4a6318dbc9cb",
+    "url": "img/DCR.e38efa7e.svg"
+  },
+  {
+    "revision": "373f90a0c904f32591f14e5c20b80716",
+    "url": "img/AID.373f90a0.svg"
+  },
+  {
+    "revision": "8be2ec978d9a4d3abc64122eb0296dab",
+    "url": "img/ESS.8be2ec97.svg"
+  },
+  {
+    "revision": "9c5b36cd79ef3657b160b1cfb7f1dc60",
+    "url": "img/coin_red_bg.9c5b36cd.svg"
+  },
+  {
+    "revision": "0641e53f1647b73607618e3f79b30286",
+    "url": "img/LTC.0641e53f.svg"
+  },
+  {
+    "revision": "852500306746cdbe1c1ecd9cf51f424f",
+    "url": "img/PIVX.85250030.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5967fb40f5bfe5feb9ebc7630c479c8d",
+    "url": "img/GRS.5967fb40.svg"
+  },
+  {
+    "revision": "c8679938929f8436b9d5727f496c8b24",
+    "url": "img/coin_green_bg.c8679938.svg"
+  },
+  {
+    "revision": "8647fed8299c41adc6262812fc0ee466",
+    "url": "img/BTC.8647fed8.svg"
+  },
+  {
+    "revision": "67692810651fb73ad71da56895b714bb",
+    "url": "img/ETH.67692810.svg"
+  },
+  {
+    "revision": "a905f01fe95cfe534cb7fa517c784312",
+    "url": "index.html"
+  },
+  {
+    "revision": "bfb0a117633576d02bfd7a0a47a37fbb",
+    "url": "fav.png"
+  },
+  {
+    "revision": "0784470071e1a768a23c",
+    "url": "css/about.15a85036.css"
+  }
+];
